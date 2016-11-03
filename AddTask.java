cdcae9a7322c8ca82ef874c2aa655f93dbbecbe6
Yutong Wang <monicawang_7@yahoo.com>
2016-11-03 18:53:23 -0500

    Add files via upload

---
+++ AddTask.java
@@ -0,0 +1,25 @@
+package com.example.yutong.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by yutong on 11/2/16.
+ */
+public class AddTask extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_addtask);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8), (int)(height*.6));
+    }
+}
